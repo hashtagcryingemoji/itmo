ba36f2dbfea86325167e46f6a4d68ed221cf7e5e
3fant <3fantimofey@gmail.com>
2026-09-21 19:20:29 +0300

    feat: csa lab3.1

--- 3sem/csa/lab3/acc32/is_prime.s
+++ 3sem/csa/lab3/acc32/is_prime.s
@@ -1,62 +1,62 @@
-.data
+    .data
 
-input_addr: .word 0x80
-output_addr: .word 0x84
-n: .word 0
-root: .word 1
-divisor: .word 2
-const_one: .word 1
+input_addr:      .word  0x80
+output_addr:     .word  0x84
+n:               .word  0
+divisor:         .word  3
+divisor_squared: .word  9
+const_one:       .word  1
+const_two:       .word  2
+is_prime_const:  .word  1
+is_not_prime_const: .word  0
+lesser_than_one_const: .word  -1
 
-.text
+    .text
 
-.org 0x88
+    .org         0x88
 
 _start:
-	load input_addr
-	load_acc
-	beqz lesser_than_one
-	bltz lesser_than_one
-	store n
-	sub const_one
-	beqz is_not_prime	 
-		
-calc_square:
-	load root
-	add const_one
-	store root
-	mul root ;acc = root^2
-	bvs loop ; handling root^2 > 2^7-1 case 
-	sub n
-	bgtz loop
-	bltz calc_square
-	load root
-	add const_one
-	store root ; root = floor(sqrt(n)) + 1 
+    load         input_addr
+    load_acc
+    beqz         lesser_than_one
+    bltz         lesser_than_one
+    store        n
+    sub          const_one
+    beqz         is_not_prime
+    sub          const_one
+    beqz         is_prime
+    add          const_two
+    rem          const_two
+    beqz         is_not_prime
+
 
 loop:
-	load root
-	sub divisor
-	beqz is_prime
-	load n
-	rem divisor
-	beqz is_not_prime
-	load divisor
-	add const_one
-	store divisor
-	jmp loop		
+    load         n
+    sub          divisor_squared
+    bltz         is_prime
+    load         n
+    rem          divisor
+    beqz         is_not_prime
+    load         divisor
+    add          const_two
+    store        divisor
+    mul          divisor
+    bvs          is_prime                    ; div > srqt(n)
+    store        divisor_squared
+    jmp          loop
 
 is_not_prime:
-	load_imm 0
-	store_ind output_addr
-	halt
+    load         is_not_prime_const
+    jmp          output
 
 is_prime:
-	load_imm 1
-	store_ind output_addr
-	halt
+    load         is_prime_const
+    jmp          output
 
 lesser_than_one:
-	load_imm -1
-	store_ind output_addr
-	halt
+    load         lesser_than_one_const
+    jmp          output
 
+output:
+    store_ind    output_addr
+    halt
